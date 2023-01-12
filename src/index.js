47220ca58a747c492687eeb39874461f6cd25b48
Vladislav Lekontsev <other2other2@gmail.com>
2023-01-12 18:34:36 +0400

    feat: do reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n === 0) return 0;
+    let str = n.toString();
+    let result = '';
+
+    for (let i = str.length - 1; i >= 0; i -= 1) {
+        if (str[i] === '-') continue;
+        result = `${result}${str[i]}`;
+    }
+    return +result;
 }
